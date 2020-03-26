6225464c2b87a57c64dc652e837e7a92e84dd590
chucksterv <deshanvikum@gmail.com>
2020-03-26 19:25:08 +1100

    Add files via upload

---
+++ buddy.ino
@@ -0,0 +1,56 @@
+int led = D6;
+
+
+void setup() {
+    
+    pinMode(led, OUTPUT);
+    
+    Particle.subscribe("Deakin_RIOT_SIT210_Photon_Buddy", myHandler);
+
+}
+
+void loop() {
+    
+    
+
+}
+
+void myHandler(const char *event, const char *data)
+{
+    if (strcmp(data,"wave")==0)
+    {
+        digitalWrite(led, HIGH);
+        delay(500);
+        digitalWrite(led, LOW);
+        delay(500);
+        
+        digitalWrite(led, HIGH);
+        delay(500);
+        digitalWrite(led, LOW);
+        delay(500);
+        
+        digitalWrite(led, HIGH);
+        delay(500);
+        digitalWrite(led, LOW);
+        delay(500);
+    }
+    
+    else if (strcmp(data,"pat")==0)
+    {
+        digitalWrite(led, HIGH);
+        delay(500);
+        digitalWrite(led, LOW);
+        delay(500);
+        
+        digitalWrite(led, HIGH);
+        delay(500);
+        digitalWrite(led, LOW);
+        delay(500);
+    }
+    
+    else
+    {
+        
+    }
+        
+}
